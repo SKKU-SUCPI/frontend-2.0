f5da28a3a837e9a287ec92dcb4a59adb8d4e0076
byungKHee <byungheekang@g.skku.edu>
2025-03-20 21:28:22 +0900

    [feat] 레이아웃 설정

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,37 @@
+/** @jsxImportSource @emotion/react */
 import { Outlet } from "react-router-dom";
+import { css } from "@emotion/react";
+import NavBar from "@/layouts/NavBar";
+import TopBar from "@/layouts/TopBar";
 
-const MainLayout = () => {
+const layoutStyle = css`
+  display: flex;
+  height: 100vh;
+`;
+
+const contentWrapperStyle = css`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+const contentStyle = css`
+  flex-grow: 1;
+  padding: 24px;
+  background-color: #ffffff;
+  overflow-y: auto;
+`;
+
+const MainLayout: React.FC = () => {
   return (
-    <div>
-      <Outlet />
+    <div css={layoutStyle}>
+      <NavBar />
+      <div css={contentWrapperStyle}>
+        <TopBar />
+        <div css={contentStyle}>
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 };
