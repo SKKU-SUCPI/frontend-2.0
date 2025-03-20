2c7e30a7c99ab11141cb0ebe1e9f1a5e66135873
byungKHee <byungheekang@g.skku.edu>
2025-03-20 21:00:08 +0900

    [feat] 라우팅 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "@/layouts/MainLayout";
 import Home from "@/components/Home";
-
+import Score from "@/components/Score";
+import Activity from "@/components/Activity";
 // TODO
 // 라우팅 경로 설정
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    children: [{ path: "/", element: <Home /> }],
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/score", element: <Score /> },
+      { path: "/activity", element: <Activity /> },
+    ],
   },
 ]);
 
