66d92924082e9347814760e055332415357c7bf1
byungKHee <byungheekang@g.skku.edu>
2025-04-10 15:40:41 +0900

    [feat] 로그인 컴포넌트 임시 제작

---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { css } from "@emotion/react";
+
+const Login: React.FC = () => {
+  return (
+    <div
+      css={css`
+        padding: 20px;
+      `}
+    >
+      <h1>Login Page</h1>
+    </div>
+  );
+};
+
+export default Login;
