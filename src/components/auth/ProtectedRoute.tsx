cf6404016702a10333d2cd8b35f79cabe587202d
byungKHee <byungheekang@g.skku.edu>
2025-04-10 15:40:01 +0900

    [feat] 권한 확인을 위한 전역 컴포넌트 추가

---
+++ src/components/auth/ProtectedRoute.tsx
@@ -0,0 +1,22 @@
+import useUserStore from "@/stores/auth/userStore";
+import { Navigate, Outlet } from "react-router-dom";
+
+interface ProtectedRouteProps {
+  allowedRoles: number[];
+}
+const ProtectedRoute = ({ allowedRoles }: ProtectedRouteProps) => {
+  const { user_id, user_role } = useUserStore();
+
+  if (!user_id) {
+    // 알림 띄우고 로그인 페이지로 리다이렉트
+    alert("로그인이 필요합니다. 로그인 페이지로 이동합니다.");
+    return <Navigate to="/login" replace />;
+  }
+
+  if (user_role && !allowedRoles.includes(user_role)) {
+    // 알림 띄우고 메인으로 리다이렉트
+    alert("권한이 없습니다. 메인 페이지로 이동합니다.");
+    return <Navigate to="/" replace />;
+  }
+  return <Outlet />;
+};
