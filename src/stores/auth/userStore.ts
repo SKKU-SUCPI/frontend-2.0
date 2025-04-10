2cd649493b6334a33a2c22b1f01ea619fa18d803
byungKHee <byungheekang@g.skku.edu>
2025-04-10 15:41:12 +0900

    [fix] 로그아웃 상태 표시를 위해 null 필드 추가

--- src/stores/auth/userStore.ts
+++ src/stores/auth/userStore.ts
@@ -1,21 +1,36 @@
 import { create } from "zustand";
 
 interface UserStore {
-  user_id: number;
-  user_role: 0 | 1 | 2;
-  user_name: string;
-  user_hakbun: string;
-  user_hakgwa_cd: string;
-  user_year: number;
+  user_id: number | null;
+  user_role: 0 | 1 | 2 | null;
+  user_name: string | null;
+  user_hakbun: string | null;
+  user_hakgwa_cd: string | null;
+  user_year: number | null;
 }
 
-const useUserStore = create<UserStore>((set) => ({
-  user_id: 0,
-  user_role: 0,
-  user_name: "김민수",
-  user_hakbun: "2020123456",
-  user_hakgwa_cd: "CS201",
-  user_year: 1.5,
+interface UserStoreActions {
+  setUser: (user: UserStore) => void;
+  clearUser: () => void;
+}
+
+const useUserStore = create<UserStore & UserStoreActions>((set) => ({
+  user_id: null,
+  user_role: null,
+  user_name: null,
+  user_hakbun: null,
+  user_hakgwa_cd: null,
+  user_year: null,
+  setUser: (user: UserStore) => set(user),
+  clearUser: () =>
+    set({
+      user_id: null,
+      user_role: null,
+      user_name: null,
+      user_hakbun: null,
+      user_hakgwa_cd: null,
+      user_year: null,
+    }),
 }));
 
 export default useUserStore;
