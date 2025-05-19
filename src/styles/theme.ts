22fead319cafaa5b6a69f7de7252b73833794363
byungKHee <byungheekang@g.skku.edu>
2025-05-20 00:44:29 +0900

    [feature] 색상 테마, 폰트 테마 정리

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,22 +2,30 @@
 import { createTheme } from "@mui/material";
 
 const customTheme = {
-  customColors: {},
-  customSpacing: {
-    small: "8px",
-    medium: "16px",
-    large: "32px",
+  customColors: {
+    lq: "#0088FE",
+    rq: "#00C49F",
+    cq: "#FFBB28",
+    approved: "#2ecc40", // 승인 - 초록
+    pending: "#ffcc00", // 대기 - 노랑
+    rejected: "#ff4d4f", // 반려 - 빨강
+    black: "000000",
+    darkGray: "#333",
+    mediumGray: "#757575",
+    lightGray: "#e0e0e0",
   },
+  customWeights: {
+    bold: "600",
+    semibold: "500",
+    normal: "400",
+  },
+  customSpacing: {},
   customFontSize: {
-    small: "14px",
-    medium: "16px",
-    large: "20px",
+    title: "2.5rem",
+    subtitle: "1.8rem",
+    itemTitle: "1.5rem",
   },
-  customBorderRadius: {
-    small: "4px",
-    medium: "8px",
-    large: "16px",
-  },
+  customBorderRadius: {},
 };
 
 const muiTheme = createTheme();
