027dc665534afb8bc4d5c0147262fca1ecfae48e
byungKHee <byungheekang@g.skku.edu>
2025-05-21 19:58:59 +0900

    feat: axios 관련 설정

---
+++ src/apis/utils/axiosInterceptor.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosInstance } from "axios";
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  timeout: 10000,
+});
+
+// 요청 인터셉터
+axiosInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    console.error("API 요청 오류:", error);
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.error("API 요청 오류:", error);
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
