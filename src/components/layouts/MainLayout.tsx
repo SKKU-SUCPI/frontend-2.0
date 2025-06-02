4bddcee7f2ef5e555b30dfc1aeb11babfefe8eee
byungKHee <byungheekang@g.skku.edu>
2025-06-02 23:18:44 +0900

    feat: 페이지 트랜지션 추가

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,7 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import NavBar from "@/components/layouts/NavBar";
+import { motion } from "framer-motion";
 
 const layoutStyle = css`
   display: flex;
@@ -13,17 +14,27 @@ const layoutStyle = css`
 const contentStyle = css`
   width: 100%;
   height: 100%;
-  // TODO: 정확한 기준으로 다시 설정할 것
   max-width: 1100px;
   margin-top: 20px;
+  position: relative; // 애니메이션 대상 요소가 겹치지 않도록
 `;
 
 const MainLayout: React.FC = () => {
+  const location = useLocation();
+
   return (
     <div css={layoutStyle}>
       <NavBar />
       <div css={contentStyle}>
-        <Outlet />
+        <motion.div
+          key={location.pathname}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.4 }}
+          exit={{ opacity: 0 }}
+        >
+          <Outlet />
+        </motion.div>
       </div>
     </div>
   );
