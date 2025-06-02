c28901f20593e54c9c7522e01e8e78cbc872f182
byungKHee <byungheekang@g.skku.edu>
2025-06-02 16:39:26 +0900

    feat: 대시보드 api 연결

---
+++ src/pages/admin/statistic/dashboard/utils/transform3qAverage.ts
@@ -0,0 +1,60 @@
+const transformTotalAverage = (data: {
+  rq: number;
+  lq: number;
+  cq: number;
+}) => {
+  if (!data) return [];
+  return [
+    {
+      name: "RQ",
+      description: "Research Quotient",
+      score: Math.round(data?.rq * 100) / 100,
+    },
+    {
+      name: "LQ",
+      description: "Learning Quotient",
+      score: Math.round(data?.lq * 100) / 100,
+    },
+    {
+      name: "CQ",
+      description: "Communication Quotient",
+      score: Math.round(data?.cq * 100) / 100,
+    },
+  ];
+};
+
+const transformDepartmentAverage = (
+  data: Record<string, { lq: number; rq: number; cq: number }>
+) => {
+  if (!data) return [];
+
+  const convertName = (name: string) => {
+    if (name === "sw") return "소프트웨어";
+    if (name === "soc") return "글로벌융합";
+    if (name === "intelligentSw") return "지능형소프트";
+    return name;
+  };
+
+  const RQ: { name: string; score: number }[] = [];
+  const LQ: { name: string; score: number }[] = [];
+  const CQ: { name: string; score: number }[] = [];
+
+  Object.entries(data).forEach(([dept, scores]) => {
+    RQ.push({
+      name: convertName(dept),
+      score: Math.round(scores.rq * 100) / 100,
+    });
+    LQ.push({
+      name: convertName(dept),
+      score: Math.round(scores.lq * 100) / 100,
+    });
+    CQ.push({
+      name: convertName(dept),
+      score: Math.round(scores.cq * 100) / 100,
+    });
+  });
+
+  return { RQ, LQ, CQ };
+};
+
+export { transformTotalAverage, transformDepartmentAverage };
