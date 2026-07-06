6bfac85ea61c87fe952edf9a7397758c2b3e6b5f
gdlee412 <gdlee412@gmail.com>
2026-07-06 14:33:44 +0900

    fix: changed joinStatus from string to int

--- src/apis/team/patchTeamJoinStatus.ts
+++ src/apis/team/patchTeamJoinStatus.ts
@@ -2,11 +2,7 @@ import axiosInstance from "@/apis/utils/axiosInterceptor";
 
 export interface PatchJoinStatusRequest {
   teamId: number;
-<<<<<<< HEAD
   joinStatus: number; // (0 = JOINED, 1 = PENDING, 2 = REJECTED)
-=======
-  joinStatus: "JOINED" | "REJECTED";
->>>>>>> 1af930c19b5a359afa26ee417dea5f8ccaae3b0b
 }
 
 const patchTeamJoinStatus = async (data: PatchJoinStatusRequest) => {
